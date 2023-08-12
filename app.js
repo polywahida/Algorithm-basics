4cf4c02dcd3cea95344623df59beb8965ce83e66
poly_wahida <ifrahpoly@gmail.com>
2023-08-13 03:10:45 +0600

    Algorithm part-3

--- app.js
+++ app.js
@@ -106,12 +106,124 @@ if(charMap[char] > max)
 { max = charMap[char];
  maxCharacter =char;
 }
-} return MaxCharacter;
+} return maxCharacter;
 
 
 
 
 algorithm = "Max-Character" ;
+
+
+let words = "Hi there"; 
+let newWords = words.replace(/[^\w]/g, ""); 
+console.log(newWords);
+
+// Steps
+
+// anagram(stringA, strings)
+
+// Helper function (buildChar(string)) // Step-1: Initialized an empty object
+
+// Step-2: Remove any special characters (including spaces) by using regular expressions // Step-3: Convert the clean string into lowercase
+
+//Step-4: Iterate over each character in the cleaned string.
+
+// Step-5: Check each character key is exists in the character 11 If it does, increment its frequency count by 1
+
+
+//If it doesn't, set its frequncey count to 1 in the 'charMap
+
+// Step-6: return characterMap object
+
+// Mainfunction Anagrass Function
+
+// Step-1: call the 'Helper function (buildChan(stringA))', store in 'charMapA' 
+// Step-2: call the 'Helper function(buildChar(string)), store in 'charMapB'
+// Step-3: Check if number of unique keys(charachter) in 'charMapA' === 'charMapB'
+// If doesn't return false
+
+// step-4: Iterate throug each charachter in 'charMapA 
+// Step-5: for each characher, check if the frequency count of charachters charmapA
+
+// If doesn't return false 
+// Step-6: After loop complete, return true;
+
+// {h:1, e:1, 1:2, 0:1) => hello
+
+// (h:1, 0:1, 1:2, 0:1, s:1) => hellos
+ function buildCharMap(str){
+
+const charMap={};
+
+for(let char of str.replace(/[^\w]/g, "").tolowerCase()){
+     charMap[char]= charMap[char] + 1 || 1;
+
+}
+
+return charMap;
+
+} 
+function CheckAnagram(stringA,_stringB){
+
+let charMapA = buildCharMap(stringA);
+
+let charMapB = buildCharMap(_stringB);
+ if(Object.keys (charMapA).length != Object.keys(charMapA).length) {
+
+
+return false;
+ }
+  for (let char in charMapA) {
+        if( charMapA[char] !== charMapB[char])
+        { return false; 
+        }
+     }
+        
+        return true;
+        
+        }
+        //arrayChunk
+
+        //1: Create empty array o hold all chunks (sub-arr)
+        //2: For Each element in the unchunked array 
+        //3: Retrive the last element in 'chunked'
+        
+        //4: If last element does not exist,
+        
+        //or if its length is equal to chunked Size 
+        //p-6: Push new chunk into 'chunked' with the current
+        // p-7: Else thge current element into the Chunk
+        
+        function ArrayChunk (arr, chunk) {
+             let chuckend = [];
+        
+        for(let element of arr) {
+        
+        let lastElement = chuckend [chuckend.length -1 ];
+        if(!lastElement || lastElement.length === chunk)
+        {chuckend.push([element]); 
+        } else {
+            lastElement.push(element);
+            
+        }
+    }
+    return words.join(' ');
+}
+
+function Capitalization(str){
+
+    let words = [];
+    
+    for(let item of str.split(' '))
+    { words.push(item[0].toUpperCase() + item.slice(1));
+    
+    return words.join(' ');
+    
+    }
+    
+   }   console.log(Capitalization ("please read carefully"));
+    
+
 document.getElementById("algorithm").innerHTML = algorithm;
 
-document.getElementById("result").innerHTML = maxCharacter("students are getting bored");
+document.getElementById("result").innerHTML = Capitalization("Please read carefully");
